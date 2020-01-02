046535c252912542b66edb5260c2fdcb449f20ca
BushraImtiaz <50461706+BushraImtiaz@users.noreply.github.com>
2020-01-02 18:50:11 +0500

    Code FestQ1

--- library.py
+++ library.py
@@ -1,44 +1,50 @@
-# Task 3:
-# Your task is to write a computer program that asks the user if they are looking for a fiction or a non fiction book.Based on the user answer the program will ask the user to choose genre from a list a list available geres.Finally the program will return the location from a to i of books of this genre.
-fiction = {
-    "A" : "COMEDY",
-    "B" :  "COMIC/GRAPHIC NOVEL",
-    "C" : "SCIENCE FICTION",
-    "D" : "FANTASY",
-    "E" : "HISTORICAL FICTION"
-}
-non_fiction ={
-    "F" : "HISTORY & GEOGRAPHY",
-    "G" : "ARTS",
-    "H" : "SCIENCE & TECHNOLOGY",
-    "I" :  "OTHER"
-}
-print("SELECT ANY ONE OPTION!")
-print("ENTER 1 FOR 'FICTION'")
-print("ENTER 2 FOR 'NON FICTION'")
-c = int(input("Enter your choice here:"))
-if c == 1:
-    print("============FICTION=================")
-    print("Choose the genre from the below list!!")
-    print("'COMEDY'")
-    print("'COMIC/GRAPHIC NOVEL'")
-    print("'SCIENCE FICTION'")
-    print("'FANTASY'")
-    print("'HISTORICAL FICTION'")
-    genre = input("Enter your choice here:")
-    genre = genre.upper()
-    for i,j in fiction.items():
-        if genre == j:
-            print("the location of this book is,",i,"shelf")
-if c == 2:
-    print("==========NON FICTION================") 
-    print("Choose the genre from the below list!!")
-    print("'HISTORY & GEOGRAPHY")
-    print("'ARTS'")
-    print("'SCIENCE & TECHNOLOGY")
-    print("'HISTORY & GEOGRAPHY")
-    genre = input("Enter your choice here:")
-    genre = genre.upper()
-    for i,j in non_fiction.items():
-        if genre == j:
-            print("the location of this book is",i,"shelf")
+# ------------- Library ----------------
+
+# def lib(ty):
+#     if ty == "Fiction" or ty == "fiction":
+#         genrefic(gen)
+#     elif ty == "Non Fiction" or ty == "non fiction":
+#         genrenonfic(gen)
+#     else:
+#         print("Invalid category entered")
+
+
+def genrefic(ge):
+            if ge == "Comedy".lower():
+                return 'A'
+            elif ge == "Comic".lower():
+                return 'B'
+            elif ge == "Sci Fi".lower():
+                return 'C' 
+            elif ge == "Fantasy".lower():
+                return 'D'
+            elif ge == "Historic Fiction".lower():
+                return 'E'
+            else:
+                print("Invalid genre. Please check in the Non-Fiction area.")
+
+def genrenonfic(ge):
+            if ge == "History or Geography".lower():
+                return 'F'
+            elif ge == "Art".lower():
+                return 'G' 
+            elif ge == "Science and Technology".lower():
+                return 'H'
+            elif ge == "Other".lower():
+                return 'I'
+            else:
+                print("Invalid genre. Please check in the Fiction area")
+
+
+ch = input("Enter whether you want to search in Fiction or Non-Fiction:  ")
+
+if ch == "fiction".lower():
+    gen = input("Enter the genre you are looking from:  ")
+    genrefic(gen)
+    print(genrefic(gen))
+elif ch == "nonfiction".lower():
+    gen = input("Enter the genre you are looking from:  ")
+    genrenonfic(gen)
+    print(genrenonfic(gen))
+else:
+    print("Incorrect Category")
